c21bed9f5fe27be2cbd2cfb681df05f8d325b39b
Javadi <javadi91m@gmail.com>
2023-02-17 08:59:43 +0100

    Child Actors

--- src/main/scala/part2actors/L3_ChildActors.scala
+++ src/main/scala/part2actors/L3_ChildActors.scala
@@ -3,26 +3,37 @@ package part2actors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import akka.actor.typed.scaladsl.Behaviors
 
-object ChildActors {
+object L3_ChildActors extends App {
 
   /*
     - actors can create other actors (children): parent -> child -> grandChild -> ...
                                                         -> child2 -> ...
+    an ActorSystem is a hierarchy of Actors creating other Actors in a tree-like structure
     - actor hierarchy = tree-like structure
-    - root of the hierarchy = "guardian" actor (created with the ActorSystem)
+
+    - the root of the whole hierarchy is called "guardian" actor (created by using ActorSystem)
     - actors can be identified via a path: /user/parent/child/grandChild/
     - ActorSystem creates
-      - the top-level (root) guardian, with children
+      - the top-level (root) guardian, with children. this guardian also contains:
         - system guardian (for Akka internal messages)
-        - user guardian (for our custom actors)
-    - ALL OUR ACTORS are child actors of the user guardian
+        - user guardian (for our custom actors) => ALL OUR ACTORS are child actors of the user guardian
+              => ActorSystem(behavior, "name"): name belongs to the system but behavior is the behavior of user guardian
+              user guardian has some important roles, so from now on, we use it only for setting up our message interactions
+        - some other guardians that we don't need in this course (for example if we want to do clustering with Akka actors)
    */
 
   object Parent {
+
+    // based on the Command, parent can spawn other child actors
+    // for spawning child actors, we use "context" in the parent
     trait Command
+
     case class CreateChild(name: String) extends Command
+
     case class TellChild(message: String) extends Command
+
     case object StopChild extends Command
+
     case object WatchChild extends Command
 
     def apply(): Behavior[Command] = idle()
@@ -31,8 +42,14 @@ object ChildActors {
       message match {
         case CreateChild(name) =>
           context.log.info(s"[parent] Creating child with name $name")
-          // creating a child actor REFERENCE (used to send messages to this child)
+          // creating a child actor REFERENCE (it's used to send messages to this child)
+          //  ActorRef[String] => String is the type of the message that this child Actor receives
+          // spawn also takes an optional Props which is basically an untyped HashMap
+          // by using this childRef, we can communicate with the child Actor.
+          // so we should save this ref (we can consider it as an internal state for the parent actor) to be able to communicate with it
           val childRef: ActorRef[String] = context.spawn(Child(), name)
+
+          // after creating the child, we change the behavior (of the parent) in a way that messages are sent to the child
           active(childRef)
       }
     }
@@ -56,14 +73,15 @@ object ChildActors {
           Behaviors.same
       }
     }
-    .receiveSignal {
-      case (context, Terminated(childRefWhichDied)) =>
-        context.log.info(s"[parent] Child ${childRefWhichDied.path} was killed by something...")
-        idle()
-    }
+      .receiveSignal {
+        case (context, Terminated(childRefWhichDied)) =>
+          context.log.info(s"[parent] Child ${childRefWhichDied.path} was killed by something...")
+          idle()
+      }
   }
 
   object Child {
+    // context.self is the ActorRef in Akka
     def apply(): Behavior[String] = Behaviors.receive { (context, message) =>
       context.log.info(s"[${context.self.path.name}] Received $message")
       Behaviors.same
@@ -72,6 +90,8 @@ object ChildActors {
 
   def demoParentChild(): Unit = {
     import Parent._
+    // user guardian has some important roles, so from now on, we use it only for setting up our message interactions
+    // this user guardian does not receive any message, so we can consider it as a Behavior[Unit] or Behavior[NotUsed]
     val userGuardianBehavior: Behavior[Unit] = Behaviors.setup { context =>
       // set up all the important actors in your application
       val parent = context.spawn(Parent(), "parent")
@@ -92,14 +112,21 @@ object ChildActors {
     system.terminate()
   }
 
+  demoParentChild()
+
   /**
    * Exercise: write a Parent_V2 that can manage MULTIPLE child actors.
+   * TellChild needs to accept a child name argument
    */
   object Parent_V2 {
     trait Command
+
     case class CreateChild(name: String) extends Command
+
     case class TellChild(name: String, message: String) extends Command
+
     case class StopChild(name: String) extends Command
+
     case class WatchChild(name: String) extends Command
 
     def apply(): Behavior[Command] = active(Map())
@@ -126,12 +153,12 @@ object ChildActors {
           Behaviors.same
       }
     }
-    .receiveSignal {
-      case (context, Terminated(ref)) =>
-        context.log.info(s"[parent] Child ${ref.path} was killed.")
-        val childName = ref.path.name
-        active(children - childName)
-    }
+      .receiveSignal {
+        case (context, Terminated(ref)) =>
+          context.log.info(s"[parent] Child ${ref.path} was killed.")
+          val childName = ref.path.name
+          active(children - childName)
+      }
   }
 
   def demoParentChild_v2(): Unit = {
@@ -154,7 +181,4 @@ object ChildActors {
     system.terminate()
   }
 
-  def main(args: Array[String]): Unit = {
-    demoParentChild_v2()
-  }
 }
