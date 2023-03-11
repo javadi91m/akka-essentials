77369df7fe275a14c231dc0880f5e583e9817788
Javadi <javadi91m@gmail.com>
2023-03-11 10:27:46 +0100

    Ask

--- src/main/scala/part5patterns/L1_AskDemo.scala
+++ src/main/scala/part5patterns/L1_AskDemo.scala
@@ -9,10 +9,17 @@ import scala.concurrent.duration._
 import utils._
 import scala.util.{Try, Success, Failure}
 
-object AskDemo {
+object L1_AskDemo {
 
+  // Ask pattern is useful for interaction patterns in the form of Request/Response.
+  // the goal of this pattern is to obtain a Future of the result which we can process later outside the context of an Actor interaction
+
+  // we're going to create an Actor that gets a request and returns a response.
+  // we send a "ComputationalTask" and we expect a "ComputationalResult"
   trait WorkProtocol
+
   case class ComputationalTask(payload: String, replyTo: ActorRef[WorkProtocol]) extends WorkProtocol
+
   case class ComputationalResult(result: Int) extends WorkProtocol
 
   object Worker {
@@ -27,24 +34,29 @@ object AskDemo {
     }
   }
 
+  // we can communicate with "Worker" without actually having any recipient Actor.
+  // we just send a message to it and then get its response as a Future
   def askSimple(): Unit = {
     // 1 - import the right package
+    // this AskPattern object contains some extension methods for an ActorSystem or an Actor Context (we'll see this in the next example)
     import akka.actor.typed.scaladsl.AskPattern._
 
     // 2 - set up some implicits
     val system = ActorSystem(Worker(), "DemoAskSimple").withFiniteLifespan(5.seconds)
-    implicit val timeout: Timeout = Timeout(3.seconds)
-    implicit val scheduler: Scheduler = system.scheduler
+    implicit val timeout: Timeout = Timeout(3.seconds) // after this duration the future gets failed
+    implicit val scheduler: Scheduler = system.scheduler // to schedule the Future for execution
 
     // 3 - call the ask method
     val reply: Future[WorkProtocol] = system.ask(ref => ComputationalTask("Trying the ask pattern, seems convoluted", ref))
-    //                                           ^ temporary actor   ^ message that gets sent to the worker == the user guardian
+    //                                           ^ temporary actor   ^ message that gets sent to the worker (in this case it's the user guardian)
 
     // 4 - process the Future
     implicit val ec: ExecutionContext = system.executionContext
     reply.foreach(println)
   }
 
+  // in this example, the user guardian is not the Actor itself but its parent!
+  // the parent creates a child, asks something, and then handle its response.
   def askFromWithinAnotherActor(): Unit = {
     val userGuardian = Behaviors.setup[WorkProtocol] { context =>
       val worker = context.spawn(Worker(), "worker")
@@ -57,6 +69,11 @@ object AskDemo {
 
       // 2 - ask
       context.ask(worker, ref => ComputationalTask("This ask pattern seems quite complicated", ref)) {
+        // here we're defining a (partial) function (we need to pass it as second argument lists
+        // this function needs to handle the response from Worker which is of type "ComputationalResult"
+        // this function needs to take this "ComputationalResult" and then return a new message of type "WorkProtocol"
+        // then we can receive this "new message" in the Actor Behavior
+
         // Try[WorkProtocol] => WorkProtocol message that will be sent TO ME later
         case Success(ComputationalResult(count)) => ExtendedComputationalResult(count, "This is pretty damn hard")
         case Failure(ex) => ExtendedComputationalResult(-1, s"Computation failed: $ex")
@@ -77,4 +94,5 @@ object AskDemo {
   def main(args: Array[String]): Unit = {
     askFromWithinAnotherActor()
   }
+
 }
